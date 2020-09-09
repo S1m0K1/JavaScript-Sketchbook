6653bcc5a31da57ef9be9dc6a2d98afeec7d19ed
S1m0K1 <simoneandpaper@gmail.com>
2020-09-09 11:04:18 +0100

    More play with JavaScript
    Finding other ways around.

--- index.js
+++ index.js
@@ -1,19 +1,3 @@
-// alert("Hello again.");
-
-// const myName = "Simone";
-// let yourName = prompt("What is your name?");
-
-// alert("Hello " + yourName + ", my name is " + myName);
-
-// Reassigning Variables
-
-// let a = 3;
-// let b = 8; 
-
-// let c = a;
-// a = b;
-// b = c;
-
 let message = prompt("Tell me something in 140 characters or less.");
 // let response = alert("You've used " + message.length + " characters. You have " + (140-message.length) + " characters remaining.");
 
@@ -29,3 +13,9 @@ let tweet = array.splice(0,140).join('');
 
 alert(tweet + "...");
 
+let message = prompt("Tell me something in 140 characters or less.");
+if (message.length > 140) {
+    alert(message.slice(0,140) + "...");
+} else {
+    alert(message);
+}
